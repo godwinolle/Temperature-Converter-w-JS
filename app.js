deff924683f12a238597fa8311eba40b985fcf80
Godwin Olletey <godwinolle@gmail.com>
2020-01-25 07:45:16 -0500

    have to add regex now and then I should be done

--- app.js
+++ app.js
@@ -1,98 +1,82 @@
-const formValue = document.querySelector('.form');
-const convert = document.querySelector('.convert');
-const button = document.querySelector('.btn');
-const answeringDisplay = document.querySelector('.answer');
+//done for the day, need to work on having the other options work
+//add regex functionality with message saying not a number
+//add mobile functionality
 
-//regex code for making sure that the user answers only numbers
-const userPattern = /^[0-9]*$/
+//see if you can fix this issue where you can't just switch an option and its conversion will
+//just come up
 
-//what the regex code is validating
-const validation = document.querySelector('.convert-from');
+//why does celcToKelv function have to be declared as a number before it works
 
+const convertFrom = document.querySelector('.convert-FROM');
+const convertTo = document.querySelector('.convert-to');
 
-//this right here checks to make sure the user is inputing numbers and nothing else 
-validation.addEventListener('keyup', (e)=>{
-    if(userPattern.test(validation.value) === false){
-        validation.style.borderColor = "red";
-    } else{
-        validation.style.borderColor = "green";
-    }
-})
-
-const option = document.querySelector('select.form-control');
-
-const otherOption = document.querySelector('#answer');
+const formula = document.querySelector('.formula');
 
 //This converts from fahrenheit to celcius
-const fahrenToCelc = (temp)=>{
-    return (temp - 32) * 5/9;
-};
+const fahrenToCelc = (temp) => (temp - 32) * 5/9;
+
 
 //This converts from fahrenheit to kelvin
-const fahrenToKelv = (temp) =>{
-    return (temp - 32) * 5/9 + 273.15;
-};
+const fahrenToKelv = (temp) => (temp - 32) * 5/9 + 273.15;
 
 //This converts from celcius to fahrenheit
-const celcToFahren = (temp) =>{
-    return (temp * 9/5) + 32;
-};
+const celcToFahren = (temp) => (temp * 9/5) + 32;
 
 //This converts from celcius to kelvin
-const celcToKelv = (temp) =>{
-    return (Number)(temp + 273.15);
-};
+const celcToKelv = (temp) => temp + 273.15;
+
 
 //This converts from kelvin to fahrenheit
-const kelvToFahren = (temp) =>{
-    return (temp - 273.15) * 9/5 + 32;
-};
+const kelvToFahren = (temp) => (temp - 273.15) * 9/5 + 32;
+
 
 //This converts from kelvin to celcius
-const kelvToCelc = (temp) =>{
-    return (temp - 273.15);
-};
+const kelvToCelc = (temp) => temp - 273.15;
 
 
-button.addEventListener('click', ()=>{
-    const temp = validation.value;
 
-    //making sure that the user enters a value in the space provided
-    if(temp === "" || temp === null){
-        alert("Please enter a value");
+convertFrom.addEventListener('keyup', () =>{
+    const temp = convertFrom.value;
+    let newTemp = convertTo.value;
+
+    const convertFromType = document.querySelector('.selection').value;
+    const convertToType = document.querySelector('.answerSelection').value;
+
+    //this is working
+    if(convertFromType === 'fahrenheit' && convertToType === 'celcius'){
+        newTemp = fahrenToCelc(temp).toFixed(2);
+        formula.textContent = `(${temp}°F - 32) * 5/9 = ${newTemp}°C`;
+        convertTo.value = `${newTemp}`;
     }
-
-    console.log(temp);
-    if(option.value === 'Fahrenheit' && otherOption.value === 'Celcius'){
-        const conversion = fahrenToCelc(temp).toFixed(2);
-        answeringDisplay.style.display = 'block';
-        answeringDisplay.classList.add('text-center');
-        answeringDisplay.textContent = `${conversion} degrees Celcius`;
-    } else if(option.value === 'Fahrenheit' && otherOption.value == 'Kelvin'){
-        const conversion = fahrenToKelv(temp).toFixed(2);
-        answeringDisplay.style.display = 'block';
-        answeringDisplay.classList.add('text-center');
-        answeringDisplay.textContent = `${conversion} degrees Kelvin`;
-    } else if(option.value === 'Celcius' && otherOption.value === 'Fahrenheit'){
-        const conversion = celcToFahren(temp).toFixed(2);
-        answeringDisplay.style.display = 'block';
-        answeringDisplay.classList.add('text-center');
-        answeringDisplay.textContent = `${conversion} degrees Fahrenheit`;
-    } else if(option.value === 'Celcius' && otherOption.value === 'Kelvin'){
-        const conversion = celcToKelv(temp).toFixed(2);
-        answeringDisplay.style.display = 'block';
-        answeringDisplay.classList.add('text-center');
-        answeringDisplay.textContent = `${conversion} degrees Kelvin`;
-    } else if(option.value === 'Kelvin' && otherOption.value === 'Fahrenheit'){
-        const conversion = kelvToFahren(temp).toFixed(2);
-        answeringDisplay.style.display = 'block';
-        answeringDisplay.classList.add('text-center');
-        answeringDisplay.textContent = `${conversion} degrees Fahrenheit`;
-    } else if(option.value === 'Kelvin' && otherOption.value === 'Celcius'){
-        const conversion = kelvToCelc(temp).toFixed(2);
-        answeringDisplay.style.display = 'block';
-        answeringDisplay.classList.add('text-center');
-        answeringDisplay.textContent = `${conversion} degrees Celcius`;
+    //this is working
+    else if(convertFromType === 'fahrenheit' && convertToType === 'kelvin'){
+        newTemp = fahrenToKelv(temp).toFixed(2);
+        formula.textContent = `(${temp}°F - 32) * 5/9 + 273.15 = ${newTemp}°K`;
+        convertTo.value = `${newTemp}`;
     }
-
+    //this is working
+    else if(convertFromType === 'celcius' && convertToType === 'fahrenheit'){
+        newTemp = celcToFahren(temp).toFixed(2);
+        formula.textContent = `(${temp}°C * 9/5) + 32 = ${newTemp}°F`;
+        convertTo.value = `${newTemp}`;
+    }
+    //this is working
+    else if(convertFromType === 'celcius' && convertToType === 'kelvin'){
+        console.log(convertFromType, convertToType);
+        newTemp = celcToKelv(temp).toFixed(2);
+        formula.textContent = `(${temp}°C + 273.15) = ${newTemp}°K`;
+        convertTo.value = `${newTemp}`;
+    }
+    //this is working
+    else if(convertFromType === 'kelvin' && convertToType === 'fahrenheit'){
+        newTemp = kelvToFahren(temp).toFixed(2);
+        formula.textContent = `(${temp}°K - 273.15) * 9/5 + 32 = ${newTemp}°F`;
+        convertTo.value = `${newTemp}`;
+    }
+    //this is working
+    else if(convertFromType === 'kelvin' && convertToType === 'celcius'){
+        newTemp = kelvToCelc(temp).toFixed(2);
+        formula.textContent = `(${temp}°K - 273.15) = ${newTemp}°C`;
+        convertTo.value = `${newTemp}`;
+    }
 });
