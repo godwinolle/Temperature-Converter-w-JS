a2cc2d348cad3eb3c7dc772355ff04d9ca4a51ff
Godwin Olletey <godwinolle@gmail.com>
2020-01-26 00:29:45 -0500

    Finished new version of the project

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//done for the day, need to work on having the other options work
-//add regex functionality with message saying not a number
 //add mobile functionality
 
 //see if you can fix this issue where you can't just switch an option and its conversion will
@@ -7,14 +5,15 @@
 
 //why does celcToKelv function have to be declared as a number before it works
 
+//the input and output field where the user sees and enters numbers
 const convertFrom = document.querySelector('.convert-FROM');
 const convertTo = document.querySelector('.convert-to');
 
+//the formula to show what the user for future calculations
 const formula = document.querySelector('.formula');
 
 //This converts from fahrenheit to celcius
 const fahrenToCelc = (temp) => (temp - 32) * 5/9;
-
 
 //This converts from fahrenheit to kelvin
 const fahrenToKelv = (temp) => (temp - 32) * 5/9 + 273.15;
@@ -23,12 +22,10 @@ const fahrenToKelv = (temp) => (temp - 32) * 5/9 + 273.15;
 const celcToFahren = (temp) => (temp * 9/5) + 32;
 
 //This converts from celcius to kelvin
-const celcToKelv = (temp) => temp + 273.15;
-
+const celcToKelv = (temp) => Number(temp) + 273.15;
 
 //This converts from kelvin to fahrenheit
 const kelvToFahren = (temp) => (temp - 273.15) * 9/5 + 32;
-
 
 //This converts from kelvin to celcius
 const kelvToCelc = (temp) => temp - 273.15;
@@ -39,6 +36,8 @@ convertFrom.addEventListener('keyup', () =>{
     const temp = convertFrom.value;
     let newTemp = convertTo.value;
 
+    /* the values are grabbed in order to see which type of temperature the user 
+    is refering to */
     const convertFromType = document.querySelector('.selection').value;
     const convertToType = document.querySelector('.answerSelection').value;
 
@@ -76,7 +75,7 @@ convertFrom.addEventListener('keyup', () =>{
     //this is working
     else if(convertFromType === 'kelvin' && convertToType === 'celcius'){
         newTemp = kelvToCelc(temp).toFixed(2);
-        formula.textContent = `(${temp}°K - 273.15) = ${newTemp}°C`;
+        formula.textContent = `${temp}°K - 273.15 = ${newTemp}°C`;
         convertTo.value = `${newTemp}`;
     }
 });
